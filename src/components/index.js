cf75870a930e156fe18a410176d9adf9425b5b78
qiuhaoxin <502967244@qq.com>
2018-04-09 11:44:24 +0800

    commit

--- src/components/index.js
+++ src/components/index.js
@@ -13,3 +13,5 @@ export {default as Select } from './select';
 export {default as DropDown} from './dropDown';
 
 export {default as Masker} from './masker';
+
+export {default as Exchange} from './exchange';
